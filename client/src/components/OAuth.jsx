528571cd1da89f43f3686d034ba9644b7244ab3d
Bibo Boom <77632683+BiboBoom10@users.noreply.github.com>
2024-07-10 21:53:41 +0300

    google authentication, private route and profile

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function OAuth() {
 
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
     const usingGoogleHandler = async () => {
         try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app);
+
+            const result = await signInWithPopup(auth, provider);
+            // console.log(result);
+            const response = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL})
+            })
+            const data = await response.json();
+            dispatch(signInSuccess(data));
+            navigate('/');
             
         } catch (error) {
             console.log('Could not login', error);
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 function Profile() {
+
+  // allow read,
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('images/.*')
+  
+  const {currentUser} = useSelector((state) => state.user); 
+
+  const fileRef = useRef(null);
+
+  const [file, setFile] = useState(undefined);
+
   return (
-    <div>Profile</div>
+    <div className='max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <input onChange={(e) => setFile(e.target.files)} type="file" ref={fileRef} hidden accept='image/*' />
+        <img onClick={() => fileRef.current.click()} className='w-24 h-24 rounded-full object-cover cursor-pointer self-center mb-4' src={currentUser.avatar} alt="profile" />
+        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
+        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
+        <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-70'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
