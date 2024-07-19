dac1f5b60b9ff2223b8a9685e12f9fe85b83ca7f
Bibo Boom <77632683+BiboBoom10@users.noreply.github.com>
2024-07-19 10:39:33 +0300

    Upload image logic

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {app} from '../firebase';
 
 function CreateListing() {
+
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  })
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  console.log(formData);
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file)
+      uploadTask.on("state_changed", (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        reject(error);
+      },
+      () => getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        resolve(downloadURL)
+      })
+      );
+    })
+  }
+  
+  const handleImageSubmit = () => {
+    if(files.length > 0 && (files.length + formData.imageUrls.length) < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+
+      const promises = [];
+
+      for(let i=0; i < files.length; i++) {
+        promises.push(storeImage(files[i]))
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({...formData, imageUrls: formData.imageUrls.concat(urls)});
+        setImageUploadError(false);
+        setUploading(false);
+      }).catch((error) => {
+        setImageUploadError('Image upload error: (2mb max per image)');
+        setUploading(false);
+      })
+    } else {
+      setImageUploadError('You can only upload 6 images');
+      setUploading(false);
+    }
+  }
+
+  const handleRemoveImage = (index) => {
+    setFormData({...formData, imageUrls: formData.imageUrls.filter((_, i) => i !== index), })
+  }
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Create a Listing</h1>
@@ -75,11 +135,25 @@ function CreateListing() {
           </p>
 
           <div className="flex gap-4">
-            <input type="file" id="images" accept='image/*' multiple className='p-3 border border-gray-300 rounded w-full' />
-            <button className='p-3 text-white bg-green-700 hover:opacity-90 disabled:opacity-70 rounded'>Upload</button>
+            <input onChange={(e) => setFiles(Array.from(e.target.files))} type="file" id="images" accept='image/*' multiple className='p-3 border border-gray-300 rounded w-full' />
+            <button disabled={uploading} onClick={handleImageSubmit} type='button' className='p-3 text-white bg-green-700 hover:opacity-90 disabled:opacity-70 rounded'>
+              {uploading ? 'Uploading ...' : 'Upload'}
+            </button>
           </div>
-        <button className='p-3 bg-slate-700 text-white rounded-lg hover:opacity-90 disabled:opacity-70'>Create Listing</button>
+          <p className='text-red-500 text-center'>{imageUploadError && imageUploadError}</p>
+
+          {
+            formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+              <div key={url} className="flex justify-between border border-gray-300 p-3 rounded-lg">
+                <img src={url} alt="listing-image" className='w-20 h-20 object-cover rounded-lg' />
+                <button type='button' onClick={() => handleRemoveImage(index)} className='text-red-500 hover:opacity-70'>Delete</button>
+              </div>
+            ))
+          }
+
+          <button className='p-3 bg-slate-700 text-white rounded-lg hover:opacity-90 disabled:opacity-70'>Create Listing</button>
         </div>
+        
       </form>
     </main>
   )
